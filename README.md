f273078b0ed49730cf0d6a95bc79fa1d3b17ca06
eugeneryoo <eugryooene@gmail.com>
2020-12-22 06:03:18 -0500

    Getting runes from client
    got the runes

--- README.md
+++ README.md
@@ -6,5 +6,15 @@ https://github.com/stirante/lol-client-java-api
 
 https://riot-api-libraries.readthedocs.io/en/latest/lcu.html
 
+https://nickcano.com/reversing-league-of-legends-client/
+
+https://lcu.vivide.re/ - LCU docs 
+
+TODO: 
+
+Hook up runesReforged.json to runes to get names instead of integers (JSON pattern matching)
+
+Spin up Discord API - check if Discord has r/w/x permissions to set runes 
+
 
 
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,23 +6,48 @@ package org.example;
 
 import com.stirante.lolclient.ClientApi;
 import com.stirante.lolclient.ClientConnectionListener;
-import generated.LolSummonerSummoner;
+import generated.*;
+
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+
+import generated.LolPerksPerkPageResource;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 
 public class App {
+
     static ClientApi api = new ClientApi();
+    //LCU client object - works like magic
 
     public static void main(String[] args) {
 
         System.out.println("running...");
 
+        /*
+        Adds a listener waiting for an instance of the League Client
+        Binds and does a thing
+         */
         api.addClientConnectionListener(new ClientConnectionListener() {
             @Override
             public void onClientConnected() {
                 System.out.println("connected");
                 whenConnected();
+                api.stop();
+
             }
 
             @Override
@@ -34,12 +59,45 @@ public class App {
 
     }
 
+    public static ClientApi getApi() {
+        return api;
+    }
+
+    /*
+    do something when connected to the client
+     */
     private static void whenConnected() {
         try {
             LolSummonerSummoner summoner = api.executeGet("/lol-summoner/v1/current-summoner", LolSummonerSummoner.class);
             //gets summoner
+            try {
+                //change pages
+                LolPerksPerkPageResource page1 =
+                        getApi().executeGet("/lol-perks/v1/currentpage", LolPerksPerkPageResource.class);
+                if (!page1.isEditable || !page1.isActive) {
+                    //get all rune pages
+                    LolPerksPerkPageResource[] pages =
+                            getApi().executeGet("/lol-perks/v1/pages", LolPerksPerkPageResource[].class);
+                    //find available pages
+                    List<LolPerksPerkPageResource> availablePages = Arrays.stream(pages).filter(p -> p.isEditable).collect(Collectors.toList());
+                    if (availablePages.size() > 0) {
 
+                        page1 = availablePages.get(0);
+                        //rune pages are accessible!
 
+                        for(Integer name : page1.selectedPerkIds){
+                            System.out.println((name));
+                        }
+
+                        System.out.println(page1.name);
+                    } else {
+                        page1 = new LolPerksPerkPageResource();
+                    }
+                }
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
